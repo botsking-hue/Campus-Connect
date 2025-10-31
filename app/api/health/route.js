92d83d9d9e4ef64c8cc3e5fb008def6ce09b0c28
AHADI JOEL <joeahangila0@gmail.com>
2025-10-31 16:59:17 +0300

    Update route.js

--- app/api/health/route.js
+++ app/api/health/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import sql from '../../../lib/database'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   try {
     // Test database connection
