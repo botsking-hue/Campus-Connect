d35aae649acf9eca48fda90ca22d818c3794d62e
AHADI JOEL <joeahangila0@gmail.com>
2025-10-31 15:58:03 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  // No need for experimental.appDir in Next.js 13.4+
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    MAIN_ADMIN_ID: process.env.MAIN_ADMIN_ID,
+    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
   },
 }
 
