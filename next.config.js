619c35a341ffe4a42cb1dd87cb36403636d6fa51
AHADI JOEL <joeahangila0@gmail.com>
2025-10-31 15:45:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    MAIN_ADMIN_ID: process.env.MAIN_ADMIN_ID,
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-  },
 }
 
 module.exports = nextConfig
